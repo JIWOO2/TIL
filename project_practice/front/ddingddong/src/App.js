c6c9e4f3ebd3bffb5e3cca8f679665147c8229ce
unknown <baejiwoo0901@gmail.com>
2022-07-27 01:32:09 +0900

    init

---
+++ project_practice/front/ddingddong/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import LogIn from './pages/User/Login/index';
+
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      
+      <LogIn/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
